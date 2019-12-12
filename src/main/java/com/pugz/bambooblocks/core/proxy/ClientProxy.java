0236e397ac6f591792d16aa0ddd8283443fd8dcf
Pugz <pugzfordayz132@gmail.com>
2019-12-11 20:48:38 -0500

    chest render undo

--- src/main/java/com/pugz/bambooblocks/core/proxy/ClientProxy.java
+++ src/main/java/com/pugz/bambooblocks/core/proxy/ClientProxy.java
@@ -1,8 +1,13 @@
 package com.pugz.bambooblocks.core.proxy;
 
 import com.pugz.bambooblocks.client.render.BambooBoatRenderer;
+import com.pugz.bambooblocks.client.render.BambooChestTileEntityRenderer;
+import com.pugz.bambooblocks.client.render.TrappedBambooChestTileEntityRenderer;
 import com.pugz.bambooblocks.common.entity.BambooBoatEntity;
+import com.pugz.bambooblocks.common.tileentity.BambooChestTileEntity;
+import com.pugz.bambooblocks.common.tileentity.TrappedBambooChestTileEntity;
 import net.minecraftforge.fml.ModList;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 public class ClientProxy extends ServerProxy {
@@ -10,8 +15,8 @@ public class ClientProxy extends ServerProxy {
     public void init() {
         RenderingRegistry.registerEntityRenderingHandler(BambooBoatEntity.class, BambooBoatRenderer::new);
         if (ModList.get().isLoaded("quark")) {
-            //ClientRegistry.bindTileEntitySpecialRenderer(BambooChestTileEntity.class, new BambooChestTileEntityRenderer());
-            //ClientRegistry.bindTileEntitySpecialRenderer(TrappedBambooChestTileEntity.class, new TrappedBambooChestTileEntityRenderer());
+            ClientRegistry.bindTileEntitySpecialRenderer(BambooChestTileEntity.class, new BambooChestTileEntityRenderer());
+            ClientRegistry.bindTileEntitySpecialRenderer(TrappedBambooChestTileEntity.class, new TrappedBambooChestTileEntityRenderer());
         }
     }
 }
